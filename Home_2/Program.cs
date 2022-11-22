538f0b15c1aaa9ad755c6f4015d1cb3f654c3a19
Anna <A9183307737@gmail.com>
2022-11-22 13:49:04 +0300

    home2

---
+++ Home_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число ");
+int fst = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число ");
+int sec = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третье число ");
+int thd = int.Parse(Console.ReadLine()!);
+
+int max = fst;
+
+if (sec > max)
+    max = sec;
+
+if (thd > max)
+    max = thd;
+
+Console.WriteLine(max);
+
