eee652a48c34a45cb250f50ed2f2217918af8d14
Anna <A9183307737@gmail.com>
2022-11-22 11:59:40 +0300

    задача4

---
+++ Lesson_1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, котора на вход принимает одно число N, а на выходе показывает 
+//все целые числа от N до -N
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
+
+int antinumber = 0 - number;
+
+while (antinumber <= number)
+{
+  Console.WriteLine(antinumber);
+  antinumber = (antinumber + 1);  
+}
