2935f3634709f732b88a096a8ad0776f8237e800
Anna <A9183307737@gmail.com>
2022-11-22 13:21:11 +0300

    ex 1

---
+++ Home_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число ");
+int fst = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число ");
+int sec = int.Parse(Console.ReadLine()!);
+
+if (fst > sec)
+{
+    Console.Write("max=");
+    Console.WriteLine(fst);
+}
+else
+{
+    Console.Write("max=");
+    Console.WriteLine(sec);
+}
