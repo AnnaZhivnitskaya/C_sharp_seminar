8353d4657984d239957039f402abb72c439467fb
Anna <A9183307737@gmail.com>
2022-11-22 11:09:48 +0300

    задача3

---
+++ Lesson_1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет выдавать названия дней недели
+//по заданному номеру
+
+Console.WriteLine("Введите номер ");
+
+int day = int.Parse(Console.ReadLine()!);
+
+if (day == 1)
+Console.WriteLine("Sunday");
+
+if (day == 2)
+Console.WriteLine("Mondey");
+
+if (day == 3)
+Console.WriteLine("Tuesday");
+
+if (day == 4)
+Console.WriteLine("Wednesday");
+
+if (day == 5)
+Console.WriteLine("Thursday");
+
+if (day == 6)
+Console.WriteLine("Friday");
+
+if (day == 7)
+Console.WriteLine("Saturday");
