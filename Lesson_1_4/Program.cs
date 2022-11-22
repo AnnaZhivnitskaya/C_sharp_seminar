b3451fac280c093b6c6866c09cce84c80af03827
Anna <A9183307737@gmail.com>
2022-11-22 12:52:01 +0300

    задача5

---
+++ Lesson_1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// написать программу вычисления модуля числа
+
+Console.WriteLine("Задайте число ");
+int num = int.Parse(Console.ReadLine()!);
+int mod;
+if (num > 0)
+    mod = num;
+else
+    mod = (0 + (-num));
+
+Console.WriteLine (mod);
