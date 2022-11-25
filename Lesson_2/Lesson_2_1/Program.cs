dd65275a8f4f7a30b965e75293322c035689df10
Anna <A9183307737@gmail.com>
2022-11-25 22:07:13 +0300

    Lesson_2_мое решение

--- Lesson_2/Lesson_2_1/Program.cs
+++ Lesson_2/Lesson_2_1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// 1. Напишите программу, которая принимает на вход трёхзначное число
 //    и на выходе показывает последнюю цифру этого числа.
+int num = new Random().Next(100, 1000);
+
+Console.WriteLine(num);
+
+int ShowLast(int num)
+{
+    int prelast = num % 100;
+    int last = num % 10;
+    return last;
+}
+
+Console.WriteLine (ShowLast(num));
