35b4cce4091a0b318b561d6138afdd5f0b9c7507
Anna <A9183307737@gmail.com>
2022-11-26 18:13:11 +0300

    end

---
+++ Lesson_2/Home_2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное
+//трёхзначное число и удаляет вторую цифру этого числа.
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+
+int DelFir(int num)
+{
+    int first = num / 100;
+    return first;
+}
+
+int DelThi(int num)
+{
+    int third = num % 10;
+    return  third;
+}
+Console.Write(DelFir(num));
+Console.WriteLine(DelThi(num));
