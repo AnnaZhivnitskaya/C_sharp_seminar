595f7978dac58b06a62383e24c890c819419d412
Anna <A9183307737@gmail.com>
2022-11-27 15:36:48 +0300

    end_Home_2_3

---
+++ Lesson_2/Home_2_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру
+//заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите любое число");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num < 100)
+
+    Console.WriteLine("третьего числа нет");
+
+else
+{
+int ShowTh(int num)
+{
+while (num >= 1000)
+{
+    int num2 = num / 10;
+    num = num2;
+}
+int num3 = num % 10;
+return num3;
+}
+Console.WriteLine(ShowTh(num));
+}
+
---
+++ Lesson_2/Home_2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер дня недели");
+int num = int.Parse(Console.ReadLine()!);
+
+string Holyday(int num)
+{
+    if (num = 6 | 7)
+    {
+        return ("выходной");
+    }
+    else
+    {
+        return ("будний");
+    }
+}
+Console.WriteLine(Holyday(num));
