ecbc8fcd619c9cafe0ca22f6294e59d3a257861e
Anna <A9183307737@gmail.com>
2022-11-30 22:15:04 +0300

    Less_3_3

--- Lesson_3/Lesson_3_3/Program.cs
+++ Lesson_3/Lesson_3_3/Program.cs
@@ -1,3 +1,10 @@
 ﻿// 3. Напишите программу, которая принимает на вход число (N) 
 //    и выдаёт таблицу квадратов чисел от 1 до N.
 
+Console.WriteLine("Введите число");
+float n = int.Parse(Console.ReadLine()!);
+
+    for (int i = 2; i <= n; i++)
+    {
+    Console.WriteLine($"{i} x {i} = {i*i}");
+    }
