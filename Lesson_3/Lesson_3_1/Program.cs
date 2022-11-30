8dc0330aa7339d24865656a8aa0087f6d890a365
Anna <A9183307737@gmail.com>
2022-11-30 21:04:03 +0300

    3_1

--- Lesson_3/Lesson_3_1/Program.cs
+++ Lesson_3/Lesson_3_1/Program.cs
@@ -1,2 +1,15 @@
 ﻿//  Напишите программу, которая по заданному номеру четверти,
 //    показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+
+int numQuatro = int.Parse(Console.ReadLine()!);
+
+    if(numQuatro == 1)
+        Console.WriteLine("x > 0, y > 0");
+     else if(numQuatro == 2)
+        Console.WriteLine("x < 0, y > 0");
+     else if(numQuatro == 3)
+        Console.WriteLine("x < 0, y < 0");
+     else if(numQuatro == 4)
+        Console.WriteLine("x > 0, y < 0");
