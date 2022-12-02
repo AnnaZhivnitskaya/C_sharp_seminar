fe838aeece7c6b1fc29609d03a0402c7ad21055e
Anna <A9183307737@gmail.com>
2022-12-02 15:54:44 +0300

    0 и 1 Lesson _4

---
+++ Lesson_4/Lesson_4_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine()!);
+
+int SumNum(int n)
+{
+    int sum = 0;
+    while(n > 0) //пока введенное число больше 0
+    {
+        n = n/=10;//делим его на 10
+        Console.WriteLine(n);
+        sum = sum + 1; //каждый раз одно действие деления приравниваем 
+                        //к 1 и суммируем в переменной
+    }
+    return sum;
+}
+
+Console.WriteLine(SumNum(n));
+
