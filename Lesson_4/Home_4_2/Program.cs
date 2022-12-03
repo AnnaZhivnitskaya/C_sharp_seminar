187311e590e8aabc027a669306c20d8ed5555167
Anna <A9183307737@gmail.com>
2022-12-03 11:31:54 +0300

    Home 4_2

--- Lesson_4/Home_4_2/Program.cs
+++ Lesson_4/Home_4_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+
+int Funck(int num)
+{
+    int sum = num % 10;
+    num -= num % 10;
+
+    while (num > 0)
+    {
+        num /= 10;
+        sum = sum + (num % 10);
+    }
+    return sum;
+}
+Console.WriteLine(Funck(num));
