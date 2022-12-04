e26988cfd00b30fd4e95165cb2290fb130f60dc1
Anna <A9183307737@gmail.com>
2022-12-04 11:42:07 +0300

    Home 4_3

--- Lesson_4/Home_4_3/Program.cs
+++ Lesson_4/Home_4_3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] collection = new int[8];
+
+void NumArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        Console.Write(collection[index] + " ");
+        
+        index++;
+    }
+    Console.WriteLine("\r");
+}
+
+NumArray(collection);
