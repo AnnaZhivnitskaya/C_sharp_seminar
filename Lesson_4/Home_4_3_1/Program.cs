d27c75b348d0b8a0fc21a5941838fb795b065094
Anna <A9183307737@gmail.com>
2022-12-05 20:55:46 +0300

    start lesson 5

--- Lesson_4/Home_4_3_1/Program.cs
+++ Lesson_4/Home_4_3_1/Program.cs
@@ -9,7 +9,7 @@ void FillArray(int[] collection)
         collection[index] = new Random().Next(1, 100);
         index++;
     }
-}
+}                                         
 
 int[] collection2 = new int[8];
 
@@ -26,3 +26,27 @@ void PrintArray(int[] collection2)
 }
 FillArray(collection2);
 PrintArray(collection2);
+
+
+//2 вариант
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] EightMass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(1, 100);
+    return arr;
+}
+
+int[] arr_1 = EightMass(int.Parse(Console.ReadLine()));
+Print(arr_1);
