a10b3340b0c8890402c120e0c1fdc1791314f25d
Anna <A9183307737@gmail.com>
2022-12-11 16:45:00 +0300

    Home 6_1 and 6_2

--- Lesson_6/Lesson_6_5/Program.cs
+++ Lesson_6/Lesson_6_5/Program.cs
@@ -3,45 +3,45 @@
 
 // -----------------------------------------------------1 вариант
 
-void Print(double[] arr)
-{
-    int size = arr.Length;
+// void Print(double[] arr)
+// {
+//     int size = arr.Length;
 
-    for (int i = 0; i < size; i++)
-    {
-        Console.Write($"{arr[i]} ");
-    }
-    Console.WriteLine();
-}
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.Write($"{arr[i]} ");
+//     }
+//     Console.WriteLine();
+// }
 
-double[] MassNums(int size, int from, int to)
-{
-    double[] arr = new double[size];
-    Random n_new = new Random();
+// double[] MassNums(int size, int from, int to)
+// {
+//     double[] arr = new double[size];
+//     Random n_new = new Random();
 
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = Math.Round(n_new.NextDouble() * (10 + 20) - 10, 2);
-    }
-    return arr;
-}
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = Math.Round(n_new.NextDouble() * (10 + 20) - 10, 2);
+//     }
+//     return arr;
+// }
 
-double[] CopyMass(double[] arr)
-{
-    int size = arr.Length;
-    double[] new_arr = new double[size];
+// double[] CopyMass(double[] arr)
+// {
+//     int size = arr.Length;
+//     double[] new_arr = new double[size];
 
-    for (int i = 0; i < size; i++)
-    {
-        new_arr[i] = arr[i];
-    }
-    return new_arr;
-}
+//     for (int i = 0; i < size; i++)
+//     {
+//         new_arr[i] = arr[i];
+//     }
+//     return new_arr;
+// }
 
-double[] arr_1 = MassNums(5, 1, 11);
-Print(arr_1);
-double[] arr_1_new = CopyMass(arr_1);
-Print(arr_1_new);
+// double[] arr_1 = MassNums(5, 1, 11);
+// Print(arr_1);
+// double[] arr_1_new = CopyMass(arr_1);
+// Print(arr_1_new);
 
 
 // --------------------------------------------------2 вариант
