7445c7aa6106373a148cf38ecb6e56f0b3dc7119
Anna <A9183307737@gmail.com>
2022-12-15 14:28:01 +0300

    new Lesson 5

--- Lesson_5/Lesson_5_2/Program.cs
+++ Lesson_5/Lesson_5_2/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу замена элементов массива:
+//    положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+
+int[] FillArray(int nums, int from, int to) // создаем функ заполнения массива
+{
+    int[] arr = new int[nums];
+    
+    for(int i = 0; i < nums; i++)
+    
+        arr[i] = new Random().Next(from, to);
+        return arr;
+    
+}                                         
+
+
+void PrintArray(int[] print) // создаем функ печати массива
+{
+    int ind = 0;
+    int count = print.Length;
+    while (ind < count)
+    {
+        Console.Write(print[ind] + " ");
+        ind++;
+    }
+    Console.WriteLine();
+}
+
+void Change(int[] exchange) // созд функ смены знака
+{
+    for(int i = 0; i < exchange.Length; i++)
+    exchange[i] = exchange[i] * (-1);
+}
+
+int[] collection = FillArray(int.Parse(Console.ReadLine()!),
+                       int.Parse(Console.ReadLine()!),
+                       int.Parse(Console.ReadLine()!)); //создаем массив, с которыми будут работать функции
+
+
+PrintArray(collection);
+Change(collection);
+PrintArray(collection);
